51fea1899635790a1e59ac26289d4be3534908b3
Giovanni-P-Meloni <gio.mel@hotmail.com>
2020-06-03 16:56:50 -0300

    Fixed Res

--- Mechanics Showcase/Assets/Scripts/LvlChoice.cs
+++ Mechanics Showcase/Assets/Scripts/LvlChoice.cs
@@ -5,8 +5,4 @@ using UnityEditor;
 //TODO: nao necessario, apenas pra embelezar o editor....
 //Basicamente melhorar a interface do Inspector da Unity
 //Fazer apenas para aprender a usar o editor de Inspector da Unity
-public class LvlChoice : EditorWindow
-{
-    public string[] whichLevel = new string[] {"Level 1", "Level 2"};
-    public int index = 0;
-}
+
